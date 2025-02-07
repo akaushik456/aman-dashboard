1b0167a978861597370d43c0cc95cf5c28bc0437
Aman Kaushik <kaushikaman239@gmail.com>
2025-02-07 17:45:31 +0530

    add-api

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/app"; 
-import "./index.css";
+  import React from "react";
+  import { createRoot } from "react-dom/client";
+  import App from "./components/app"; 
+  import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
+  const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <React.StrictMode>
-    <App /> {/* ✅ Only App is rendered, routing will handle pages */}
-  </React.StrictMode>
-);
+  root.render(
+    <React.StrictMode>
+      <App /> {/* ✅ Only App is rendered, routing will handle pages */}
+    </React.StrictMode>
+  );
