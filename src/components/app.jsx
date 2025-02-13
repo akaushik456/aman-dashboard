cf34b96b808bdde5bc33809982b677be5ef7e364
Aman Kaushik <kaushikaman239@gmail.com>
2025-02-13 14:51:33 +0530

    update on index

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./login"; 
 import Dashboard from "./dashboard"; 
 import Categories from "./categories";
